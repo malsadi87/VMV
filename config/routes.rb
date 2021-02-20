340a31baeae8603e6e054384de5a58bda84c3749
malsadi87 <mehmet.alsadi@gmail.com>
2021-02-20 14:18:29 +0300

    Main controller added
    Main controller and its views are added.
    Bootstrap is configured.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  #get 'main/index'
+  get "/main", to: "main#index"
+  get "main/verifiable"
+  get "main/elections"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
